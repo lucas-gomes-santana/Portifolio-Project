7015b494a45e3e6265aa6af827b416343e8c3522
Lucas Gomes Santana <ls7128387@gmail.com>
2025-02-22 21:28:18 -0300

    Solving bugs

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,13 +2,31 @@ import { useNavigate } from 'react-router-dom';
 
 import '../css/Projects.css';
 
-function Projects(){
+interface ProjectElements {
+    image:string;
+    title:string;
+    description:string;
+    link:string;
+    projectType:string;
+}
+
+function Projects({ image,title,description,link,projectType }: ProjectElements){
 
     const navigate = useNavigate();
 
     return (
         <section>
             <h2>My Projects:</h2>
+
+            <div className="card-projects">
+                <h3>{ title }</h3>
+                <h4>{ projectType }</h4>
+                <img src={ image } alt="" />
+                <p>{ description }</p>
+                <a rel='noopener noreferrer' target='_blank' href={ link }>
+                    Get the Source Code
+                </a>
+            </div>
 
             <button onClick={() => navigate(-1)}>Return to Main Page</button>
         </section>
