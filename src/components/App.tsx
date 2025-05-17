4e8cce1d484060da73dc683ad70c3df0de906845
Lucas Gomes Santana <ls7128387@gmail.com>
2025-05-17 09:44:19 -0300

    Changing Router path on App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import Header from './Header.tsx';
 
 function App() {
     return (
-        <Router>
+        <Router basename="/Portifolio-Project">
             <Header/>
         </Router>
     );
